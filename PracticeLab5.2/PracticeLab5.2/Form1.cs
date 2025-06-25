b77b85ba8efa785d264a448a4e17eb0235a467da
Dmytro Havano <gavanyo0811@gmail.com>
2025-06-25 22:42:40 +0300

    lab5

---
+++ PracticeLab5.2/PracticeLab5.2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticeLab5._2
+{
+    public partial class Form1 : Form
+    {
+        const int A = 5;       // Кількість гілок з однієї точки (пучок)
+        const int K = 6;       // Рекурсивна глибина (порядок фрактала)
+        const float angleSpread = 60f; // Кутовий розкид між гілками
+        const float scale = 0.6f;      // Коефіцієнт зменшення довжини гілки
+        const float initialLength = 100f;
+
+        public Form1()
+        {
+            this.Text = "Фрактал Папороть";
+            this.Size = new Size(500, 300);
+            this.BackColor = Color.White;
+            this.DoubleBuffered = true;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+
+            // Початкова точка - центр нижнього краю
+            PointF start = new PointF(this.ClientSize.Width / 2, this.ClientSize.Height - 20);
+            DrawFern(g, start, -90f, initialLength, K); // -90 градусів вгору
+        }
+
+        void DrawFern(Graphics g, PointF p1, float angle, float length, int depth)
+        {
+            if (depth == 0) return;
+
+            // Малюємо A гілок
+            for (int i = 0; i < A; i++)
+            {
+                // Розрахунок кута для кожної гілки
+                float angleOffset = -angleSpread / 2 + i * (angleSpread / (A - 1));
+                float newAngle = angle + angleOffset;
+
+                // Обчислення нової точки
+                float rad = newAngle * (float)Math.PI / 180f;
+                PointF p2 = new PointF(
+                    p1.X + (float)(length * Math.Cos(rad)),
+                    p1.Y + (float)(length * Math.Sin(rad))
+                );
+
+                // Колір для кожної гілки
+                Color color = ColorFromHue((360f / A) * i);
+                using (Pen pen = new Pen(color, 1))
+                {
+                    g.DrawLine(pen, p1, p2);
+                }
+
+                // Рекурсивний виклик
+                DrawFern(g, p2, newAngle, length * scale, depth - 1);
+            }
+        }
+
+        Color ColorFromHue(float hue)
+        {
+            return ColorFromHSV(hue, 1.0, 1.0);
+        }
+
+        // HSV → RGB перетворення
+        public static Color ColorFromHSV(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = (int)value;
+            int p = (int)(value * (1 - saturation));
+            int q = (int)(value * (1 - f * saturation));
+            int t = (int)(value * (1 - (1 - f) * saturation));
+
+            switch (hi)
+            {
+                case 0: return Color.FromArgb(255, v, t, p);
+                case 1: return Color.FromArgb(255, q, v, p);
+                case 2: return Color.FromArgb(255, p, v, t);
+                case 3: return Color.FromArgb(255, p, q, v);
+                case 4: return Color.FromArgb(255, t, p, v);
+                default: return Color.FromArgb(255, v, p, q);
+            }
+        }
+    }
+}
---
+++ PracticeLab5/PracticeLab5/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace PracticeLab5
+{
+    partial class Form1 : Form
+    {
+        // Контрольні точки та вектори
+        PointF P1 = new PointF(100, 300);
+        PointF P2 = new PointF(400, 100);
+        PointF V1 = new PointF(100, -200);
+        PointF V2 = new PointF(100, 200);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Крива Ерміта";
+            this.Size = new Size(600, 500);
+            this.BackColor = Color.White;
+            this.DoubleBuffered = true;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+
+            // Малюємо контрольні точки та вектори
+            DrawControlPoints(g);
+            DrawHermiteCurve(g);
+        }
+
+        void DrawControlPoints(Graphics g)
+        {
+            Pen pen = new Pen(Color.Gray, 1) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash };
+            Brush brush = Brushes.Red;
+
+            g.FillEllipse(brush, P1.X - 4, P1.Y - 4, 8, 8);
+            g.FillEllipse(brush, P2.X - 4, P2.Y - 4, 8, 8);
+
+            g.DrawLine(pen, P1, new PointF(P1.X + V1.X / 4, P1.Y + V1.Y / 4));
+            g.DrawLine(pen, P2, new PointF(P2.X + V2.X / 4, P2.Y + V2.Y / 4));
+        }
+
+        void DrawHermiteCurve(Graphics g)
+        {
+            Pen pen = new Pen(Color.Blue, 2);
+            PointF prev = Hermite(0);
+
+            for (float t = 0.01f; t <= 1.0f; t += 0.01f)
+            {
+                PointF curr = Hermite(t);
+                g.DrawLine(pen, prev, curr);
+                prev = curr;
+            }
+        }
+
+        PointF Hermite(float t)
+        {
+            float h1 = 2 * t * t * t - 3 * t * t + 1;
+            float h2 = t * t * t - 2 * t * t + t;
+            float h3 = -2 * t * t * t + 3 * t * t;
+            float h4 = t * t * t - t * t;
+
+            float x = h1 * P1.X + h2 * V1.X + h3 * P2.X + h4 * V2.X;
+            float y = h1 * P1.Y + h2 * V1.Y + h3 * P2.Y + h4 * V2.Y;
+
+            return new PointF(x, y);
+        }
+    }
+}
